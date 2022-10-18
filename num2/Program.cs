3c7352951135126d4517b593492dcf48b6f27285
Ivan <grabovivan@gmail.com>
2022-10-18 19:36:52 +0300

    num2 added

---
+++ num2/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12 */
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(figuresSum(num));
+
+int figuresSum(int a)
+{
+    a = Math.Abs(a);
+    int result = 0;
+    while (a > 0)
+    {
+        result += a % 10;
+        a /= 10;
+    }
+    return result;
+}
